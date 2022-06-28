b53bc1fe21b25b8284094f95240a08464a480f3f
Brad DeFauw <badefauw@gmail.com>
2022-06-28 14:02:48 -0500

    pulled temps and weather descriptions from api. created variable conversions for fahrenheit and celsius from kelvin

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ console.log("hello weather app!");
 let locInput = document.getElementById("location");
 let submitBtn = document.getElementById("submit");
 
-submitBtn.addEventListener("click", getLocation)
+submitBtn.addEventListener("click", getWeather)
 
-async function getLocation() {
+async function getWeather() {
     const searchText = locInput.value;
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchText}&APPID=fc0c7dcaadfcedd322c65a4761888bed`);
     const weatherData = await response.json();
     console.log(weatherData);
+    let kelvin = weatherData.main.temp;
+    let fahrenheit = Math.round((kelvin - 273.15) * 9/5 +32);
+    let celsius = Math.round(kelvin - 273.15);
+    let mainWeather = weatherData.weather[0].main;
+    let description = weatherData.weather[0].description;
+    console.log(fahrenheit, celsius, mainWeather, description);
 }
